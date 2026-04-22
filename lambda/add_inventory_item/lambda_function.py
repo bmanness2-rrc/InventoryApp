0a39b64a0bc11b9e52f437447f4704f64396b92a
Brody Manness <bmanness2@academic.rrc.ca>
2026-04-22 00:39:49 -0500

    Feat: Added the code to each lambda function file.

---
+++ lambda/add_inventory_item/lambda_function.py
@@ -0,0 +1,38 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # Parse incoming JSON data
+    try:
+        data = json.loads(event['body'])
+    except KeyError:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Bad request. Please provide the data.")
+        }
+
+    # DynamoDB setup
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Inventory')
+
+    # Insert data into DynamoDB
+    try:
+        table.put_item(
+            Item={
+                'id': data['id'],
+                'location_id': data['location_id'],
+                'name': data['name'],
+                'description': data['description'],
+                'qty': data['qty'],
+                'price': data['price']
+            }
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f"Item with ID {data['id']} added successfully.")
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Error adding item: {str(e)}")
+        }
